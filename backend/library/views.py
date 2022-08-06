ddb6f00da9c9aa7d493c9d010c63e814e357337c
Home <naloiko52@yandex.ru>
2022-08-06 13:30:25 +0300

    Home Works 1

---
+++ backend/library/views.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializer import UserModelSerializer
+
+class UserModelViewSet(ModelViewSet):
+    serializer_class = UserModelSerializer
+    queryset = User.objects.all()
+
+
+
